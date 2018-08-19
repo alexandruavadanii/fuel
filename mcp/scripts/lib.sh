c47e5243b5369c9f471572c1b76094d31b1a986f
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-08-19 08:23:52 +0200

    add pxebr docker br
    Change-Id: I18d7eceabb756594a4adf42b71c0a45de34e4eff

--- mcp/scripts/lib.sh
+++ mcp/scripts/lib.sh
@@ -399,12 +399,16 @@ function create_veth_pairs {
 #FIXME: check already exists
   sudo ip link add veth_mcp0 type veth peer name veth_mcp1 || true
   sudo ip link add veth_mcp2 type veth peer name veth_mcp3 || true
+  sudo ip link add veth_mcp4 type veth peer name veth_mcp5 || true
   sudo ip link set veth_mcp0 up
   sudo ip link set veth_mcp1 up
   sudo ip link set veth_mcp2 up
   sudo ip link set veth_mcp3 up
+  sudo ip link set veth_mcp4 up
+  sudo ip link set veth_mcp5 up
   sudo brctl addif mcpcontrol veth_mcp0 || true
   sudo brctl addif "${vnode_networks[1]}" veth_mcp2 || true
+  sudo brctl addif "${vnode_networks[0]}" veth_mcp4 || true
 }
 
 function create_vms {
