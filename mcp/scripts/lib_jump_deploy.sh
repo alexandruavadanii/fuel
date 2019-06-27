c8e97f8b19e86032a18c957fd3b1504cc7771aa2
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2019-06-28 00:17:58 +0200

    ooooare
    Change-Id: I8e3315d73791583f3bf0cb57635887393c5b1fe1

--- mcp/scripts/lib_jump_deploy.sh
+++ mcp/scripts/lib_jump_deploy.sh
@@ -142,6 +142,8 @@ function __mount_image {
     sudo umount -l "${OPNFV_MNT_DIR}"
     # AArch64 CentOS cloud image has root partition at index 4 instead of 1
     sudo mount "${OPNFV_MAP_DEV/p1/p4}" "${OPNFV_MNT_DIR}"
+    sudo sed -i -e 's/^\(SELINUX\)=.*$/\1=permissive/g' \
+      "${OPNFV_MNT_DIR}/etc/sysconfig/selinux"
   else
     # grub-update does not like /dev/nbd*, so use a loop device to work around it
     sudo losetup "${OPNFV_LOOP_DEV}" "${OPNFV_MAP_DEV}"
